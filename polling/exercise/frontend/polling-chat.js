309188c4137f1207b8c5c65aa064ac8e475dc595
Ahmed Elhady <signup.hady@gmail.com>
2023-01-14 00:48:52 +0200

    Add long polling with setTimeout

--- polling/exercise/frontend/polling-chat.js
+++ polling/exercise/frontend/polling-chat.js
@@ -16,12 +16,37 @@ chat.addEventListener("submit", function (e) {
 
 async function postNewMsg(user, text) {
   // post to /poll a new message
-  // write code here
+  const data = { user, text };
+  const options = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  try {
+    const res = await fetch("/poll", options);
+    const json = await res.json();
+    console.log(json);
+  } catch (e) {
+    console.error("post polling error");
+  }
 }
 
 async function getNewMsgs() {
   // poll the server
-  // write code here
+  let json;
+  try {
+    const res = await fetch("/poll");
+    json = await res.json();
+  } catch (e) {
+    console.error("polling error", e);
+  }
+  allChat = json.msg;
+  render();
+  setTimeout(() => {
+    getNewMsgs();
+  }, INTERVAL);
 }
 
 function render() {
@@ -39,3 +64,41 @@ const template = (user, msg) =>
 
 // make the first request
 getNewMsgs();
+
+/**
+ * A function to demonstrate the real usage of long polling.
+ * Mindset:
+ * 1- Client sends a request to the server with a msg.
+ * 2- Server will not close the connection until it has msg to send.
+ * 3- when a message appear, the server sends it to the client and end the connection.
+ * 4- the client immediately sends a new request.
+ * The situation when the browser sent a request and has a pending connection with the server, is standard for this method. Only when a message is delivered, the connection is reestablished.
+ * If the connection is lost, because of, say, a network error, the client immediately sends a new request.
+ */
+
+async function subscribe() {
+  try {
+    const response = await fetch("/msgs");
+    if (response.status === 502) {
+      // connection timeout or error.
+      // may happen when the connection was too pending for too long.
+      // and the remote server closed it.
+      // we need to reconnect again.
+      await subscribe();
+    } else if (response.status !== 200) {
+      // An error occurred, log it.
+      console.error(`Error occurred ${response.statusText}`);
+      // reconnect in one second.
+      await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+      await subscribe();
+    } else {
+      // log the message
+      const message = await response.body();
+      console.log(message);
+      // get the next message
+      await subscribe();
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
